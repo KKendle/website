95363cb394eda62cc5709cc6452f1a79c1167491
Kevin Kendle <kevin.kendle09@gmail.com>
2021-01-30 01:20:27 -0600

    remove gtm and ga

--- wp-content/themes/kkendle/header.php
+++ wp-content/themes/kkendle/header.php
@@ -16,22 +16,11 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-NRCHT5Q');</script>
-  <!-- End Google Tag Manager -->
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRCHT5Q"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
+    <?php wp_body_open(); ?>
 
   <div id="page" class="site">
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'kkendle' ); ?></a>
@@ -48,9 +37,7 @@
                */
               ?>
               <div class="c-siteBranding">
-                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="c-siteBranding__title h1">
-                  Kevin Kendle
-                </a>
+                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="c-siteBranding__title h1">Kevin Kendle</a>
                 <p class="c-siteBranding__description">
                   <?php bloginfo('description'); ?>
                 </p>
